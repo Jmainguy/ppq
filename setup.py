c0aa9dad74aa10ed70cddeb61aca4a1b03609123
Jonathan Mainguy <jon@soh.re>
2015-11-01 16:07:50 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ppq',
+      version='0.1',
+      license='GPLv2',
+      description='An apt clone of rpm -qa and -ql from rpm based systems',
+      author='Jonathan Mainguy',
+      author_email='jon@soh.re',
+      url='https://github.com/Jmainguy/ppq',
+      scripts=['ppq'],
+     )
